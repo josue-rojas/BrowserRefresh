37d592a7be39d676dd20a39ea1ca1c63d817be37
josue rojas <josuerojas.rojas@gmail.com>
2019-01-07 22:42:01 -0500

    update script

--- index.js
+++ index.js
@@ -3,32 +3,37 @@ let chokidar = require('chokidar');
 let applescript = require('applescript');
 const path = require('path');
 
-module.exports.BrowserRefresh = function(){
-  const PORT = process.env.PORT || 8000;
+function refresh(app) {
+  const refreshScript = `
+    tell application "Safari" to activate
+    tell application "System Events"
+      delay 0
+      key code 15 using {command down} # ⌘R/⇧⌘R
+    end tell
+    tell application "${app}" to activate
+  `;
+  applescript.execString ( refreshScript );
+}
+
+// function parameters
+// filepath:
+module.exports = function(filepath, port, app){
+  const PORT = port || 8000;
+  let mainApp = app || 'atom';
   let filePath = process.argv[2] || '.'
-  // console.log(require.main)
-  // console.log(path.dirname(require.main.filename));
+  console.log('Port', PORT);
+  console.log('FilePath', filePath);
 
   // run a python server
-  exec(`cd ${filePath};open https://localhost:${PORT};python -m SimpleHTTPServer ${PORT}`, function(stdout, stderr){
+  exec(`cd ${filePath};open http://localhost:${PORT};python -m SimpleHTTPServer ${PORT}`, function(stdout, stderr){
     console.log('out', stdout);
     console.log('err', stderr);
   });
 
   //
-  chokidar.watch('${filePath}', {ignored: /(^|[\/\\])\../}).on('change', (path) => {
+  chokidar.watch(`${filePath}`, {ignored: /(^|[\/\\])\../}).on('change', (path) => {
+    console.log(`Changed ${path}`);
+    refresh(app);
   });
   //
-  function refresh() {
-    const refreshScript = `
-      tell application "Safari" to activate
-      tell application "System Events"
-        delay 0
-        key code 15 using {command down} # ⌘R/⇧⌘R
-      end tell
-      tell application "Atom" to activate
-    `;
-    // #${ focus ? '' : `tell application "${app}" to activate` }
-    applescript.execString ( refreshScript );
-  }
 }
